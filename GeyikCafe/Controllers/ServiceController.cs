0122cbf81eb055b2ce01b9edc0b015eb0669674f
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-09-23 03:00:15 +0300

    .

---
+++ GeyikCafe/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeyikCafe.Entities;
+using GeyikCafe.Context;
+
+namespace GeyikCafe.Controllers
+{
+    public class ServiceController : Controller
+    {
+        GeyikCafeContext db = new GeyikCafeContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult ServiceHeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
