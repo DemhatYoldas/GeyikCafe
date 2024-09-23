000346a9686ab9aad3fabd602cce432cf22d816b
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-09-23 03:42:55 +0300

    .

---
+++ GeyikCafe/Controllers/AdminLayoutController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeyikCafe.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        // GET: AdminLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialLogo()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialJS()
+        {
+            return PartialView();
+        }
+    }
+}
